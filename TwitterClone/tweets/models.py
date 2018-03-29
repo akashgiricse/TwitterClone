303ae3330da580f2e361ca76665aa5baf25cf3de
akashgiricse <akashgiricse@gmail.com>
2018-03-29 17:34:04 +0530

    Tweets are connected to 1st user.

--- TwitterClone/tweets/models.py
+++ TwitterClone/tweets/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Tweet(models.Model):
     content = models.CharField(max_length=140)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return str(self.content)
