afe392a0a6a52eb15c3189f78befb8cac6f4a5d0
akashgiricse <akashgiricse@gmail.com>
2018-03-31 17:57:03 +0530

    Add class based view

--- TwitterClone/tweets/urls.py
+++ TwitterClone/tweets/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-from .views import tweet_detail_view, tweet_list_view
+# from .views import tweet_detail_view, tweet_list_view
+from .views import TweetDetailView, TweetListView
 
 
 urlpatterns = [
-    url(r'^$', tweet_list_view, name='list'),
-    url(r'^1$', tweet_detail_view, name='detail'),
+    # url(r'^$', tweet_list_view, name='list'),
+    # url(r'^1$', tweet_detail_view, name='detail'),
+    url(r'^$', TweetDetailView.as_view(), name='list'),
+    url(r'^1$', TweetListView.as_view(), name='detail'),
+
+
 ]
--- TwitterClone/tweets/views.py
+++ TwitterClone/tweets/views.py
@@ -1,28 +1,43 @@
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.shortcuts import render
 from .models import Tweet
 # Create your views here.
 
 
-def tweet_detail_view(request, id=1):
-
-    obj = Tweet.objects.get(id=id)  # GET from database
-    print(obj)
-    context = {
-        "object": obj
-    }
-
-    return render(request, "tweets/detail_view.html", context)
-
-
-def tweet_list_view(request):
-
+class TweetDetailView(DetailView):
+    template_name = "tweets/detail_view.html"
     queryset = Tweet.objects.all()
-    print(queryset)
-    for obj in queryset:
-        print(obj.content)
-    context = {
-        "object_list": queryset
 
-    }
+    def get_object(self):
+        return Tweet.objects.get(id=1)
 
-    return render(request, "tweets/list_view.html", context)
+
+class TweetListView(ListView):
+    template_name = "tweets/list_view.html"
+    queryset = Tweet.objects.all()
+
+
+# def tweet_detail_view(request, id=1):
+
+#     obj = Tweet.objects.get(id=id)  # GET from database
+#     print(obj)
+#     context = {
+#         "object": obj
+#     }
+
+#     return render(request, "tweets/detail_view.html", context)
+
+
+# def tweet_list_view(request):
+
+#     queryset = Tweet.objects.all()
+#     print(queryset)
+#     for obj in queryset:
+#         print(obj.content)
+#     context = {
+#         "object_list": queryset
+
+#     }
+
+#     return render(request, "tweets/list_view.html", context)
