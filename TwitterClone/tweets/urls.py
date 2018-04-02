2cd77d72b03c55887345892710b657fe2ec4ce49
akashgiricse <akashgiricse@gmail.com>
2018-04-02 23:28:00 +0530

    Fix url error

--- TwitterClone/tweets/urls.py
+++ TwitterClone/tweets/urls.py
@@ -6,6 +6,6 @@ from .views import TweetDetailView, TweetListView
 urlpatterns = [
     # url(r'^$', tweet_list_view, name='list'),
     # url(r'^1$', tweet_detail_view, name='detail'),
-    url(r'^$', TweetDetailView.as_view(), name='list'),
-    url(r'^(?P<abc>\d+)/$', TweetListView.as_view(), name='detail'),
+    url(r'^$', TweetListView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='list'),
 ]
