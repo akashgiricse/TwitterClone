62220da1490b63f25c541938c9ddf1acfe1a676e
akashgiricse <akashgiricse@gmail.com>
2018-04-02 23:41:44 +0530

    Add TweetModelForm

--- TwitterClone/tweets/admin.py
+++ TwitterClone/tweets/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-
+from .forms import TweetModelForm
 from .models import Tweet
 
 
 admin.site.register(Tweet)
+
+
+class TweetModelAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Tweet
+        form = TweetModelForm
+
+
+admin.site.register(Tweet, TweetModelAdmin)
---
+++ TwitterClone/tweets/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Tweet
+
+
+class TweetModelForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
