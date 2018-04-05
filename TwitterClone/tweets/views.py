690f4ea1475f02ce8b07e149909919d127175fba
akashgiricse <akashgiricse@gmail.com>
2018-04-05 23:36:30 +0530

    Create function based tweet_create_view

--- TwitterClone/tweets/views.py
+++ TwitterClone/tweets/views.py
@@ -9,10 +9,24 @@ from .models import Tweet
 class TweetCreateView(CreateView):
     form_class = TweetModelForm
     template_name = "tweets/create_view.html"
+    success_url = "/tweet/create/"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TweetCreateView, self).form_valid(form)
+
+
+def tweet_create_view(request):
+    form = TweetModelForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.user = request.user
+        instance.save()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'tweets/create_view.html', context)
 
 
 class TweetDetailView(DetailView):
