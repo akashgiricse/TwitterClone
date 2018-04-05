23a5621aee0c56af48b54d6f964a8175b65e09c9
akashgiricse <akashgiricse@gmail.com>
2018-04-05 23:05:19 +0530

    Remove unused code

--- TwitterClone/tweets/views.py
+++ TwitterClone/tweets/views.py
@@ -1,5 +1,3 @@
-from django import forms
-from django.forms.utils import ErrorList
 from django.views.generic import DetailView, ListView, CreateView
 from django.shortcuts import render, get_object_or_404
 from .forms import TweetModelForm
@@ -13,27 +11,6 @@ class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = "/tweet/create"
-
-    # def form_valid(self, form):
-    #     if self.request.user.is_authenticated():
-    #         form.instance.user = self.request.user
-    #         return super(TweetCreateView, self).form_valid(form)
-    #     else:
-    #         form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["User must be logged in."])
-    #         return self.form_invalid(form)
-
-
-# def tweet_create_view(request):
-#     form = TweetModelForm(request.POST or None)
-#     if form.is_valid():
-#         instance = form.save(commit=False)
-#         instance.user = request.user
-#         instance.save()
-
-#     context = {
-#         "form": form
-#     }
-#     return render(request, 'tweets/create_view.html', context)
 
 
 class TweetDetailView(DetailView):
