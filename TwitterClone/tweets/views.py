829c7a58dbf21be0ea07166013f8714d852b254e
akashgiricse <akashgiricse@gmail.com>
2018-04-05 22:42:10 +0530

    Fix TweetCreateView success_url

--- TwitterClone/tweets/views.py
+++ TwitterClone/tweets/views.py
@@ -8,25 +8,25 @@ from .models import Tweet
 # Create
 class TweetCreateView(CreateView):
     form_class = TweetModelForm
-    template_name = "tweets/create_view.html"
-    success_url = "/tweet/create/"
+    template_name = 'tweets/create_view.html'
+    success_url = "/tweet/create"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TweetCreateView, self).form_valid(form)
 
 
-def tweet_create_view(request):
-    form = TweetModelForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.user = request.user
-        instance.save()
+# def tweet_create_view(request):
+#     form = TweetModelForm(request.POST or None)
+#     if form.is_valid():
+#         instance = form.save(commit=False)
+#         instance.user = request.user
+#         instance.save()
 
-    context = {
-        "form": form
-    }
-    return render(request, 'tweets/create_view.html', context)
+#     context = {
+#         "form": form
+#     }
+#     return render(request, 'tweets/create_view.html', context)
 
 
 class TweetDetailView(DetailView):
