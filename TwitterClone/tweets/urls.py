2bf7f4a78abe97c214d6542ecd19af119f49ac4f
akashgiricse <akashgiricse@gmail.com>
2018-04-09 00:02:55 +0530

    Fixed url name error

--- TwitterClone/tweets/urls.py
+++ TwitterClone/tweets/urls.py
@@ -12,9 +12,9 @@ from .views import (
 urlpatterns = [
     # url(r'^$', tweet_list_view, name='list'),
     # url(r'^1$', tweet_detail_view, name='detail'),
-    url(r'^$', TweetListView.as_view(), name='detail'),
+    url(r'^$', TweetListView.as_view(), name='list'),
     url(r'^create$', TweetCreateView.as_view(), name='create'),
-    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
     url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),
 ]
