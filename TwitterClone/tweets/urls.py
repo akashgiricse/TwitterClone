8dffcc2e86d720f86862855f1bbdef42e91e55be
akashgiricse <akashgiricse@gmail.com>
2018-04-11 23:37:58 +0530

    Change urls

--- TwitterClone/tweets/urls.py
+++ TwitterClone/tweets/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 # from .views import tweet_detail_view, tweet_list_view
+from django.views.generic.base import RedirectView
 from .views import (
     TweetDetailView,
     TweetListView,
@@ -10,9 +11,8 @@ from .views import (
 
 
 urlpatterns = [
-    # url(r'^$', tweet_list_view, name='list'),
-    # url(r'^1$', tweet_detail_view, name='detail'),
-    url(r'^$', TweetListView.as_view(), name='list'),
+    url(r'^$', RedirectView.as_view(url="/")),
+    url(r'^search/$', TweetListView.as_view(), name='list'),
     url(r'^create$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
