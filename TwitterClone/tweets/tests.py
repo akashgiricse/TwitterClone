fac28f7bfdcebea3fabb3dd1b86347758c5688de
akashgiricse <akashgiricse@gmail.com>
2018-04-14 23:55:44 +0530

    Add test

--- TwitterClone/tweets/tests.py
+++ TwitterClone/tweets/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from .models import Tweet
 # Create your tests here.
 
@@ -8,7 +9,24 @@ User = get_user_model()
 
 class TweetModelTestCase(TestCase):
     def setUp(self):
-        Tweet.objects.create(
+        some_random_user = User.objects.create(username='akashgiricseeeeeee')
+
+    def test_tweet_item(self):
+        obj = Tweet.objects.create(
             user=User.objects.first(),
             content='Some random content'
         )
+
+        self.assertTrue(obj.content == "Some random content")
+        self.assertTrue(obj.id == 1)
+
+        absolute_url = reverse("tweet:detail", kwargs={"pk": 1})
+        self.assertEqual(obj.get_absolute_url(), absolute_url)
+
+    def test_tweet_url(self):
+        obj = Tweet.objects.create(
+            user=User.objects.first(),
+            content='Some random content'
+        )
+        absolute_url = reverse("tweet:detail", kwargs={"pk": obj.pk})
+        self.assertEqual(obj.get_absolute_url(), absolute_url)
