a47abfa2845c0e05251d400f0186380ad4f26fda
akashgiricse <akashgiricse@gmail.com>
2018-04-14 23:34:18 +0530

    Add crispy form

--- TwitterClone/TwitterClone/settings/local.py
+++ TwitterClone/TwitterClone/settings/local.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'crispy_forms',
     'tweets',
 ]
 
@@ -128,3 +128,6 @@ STATICFILES_DIRS = [
 
 # will be served
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
