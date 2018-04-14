c33380c09da3de89de5837073d1c4e8f19e80846
akashgiricse <akashgiricse@gmail.com>
2018-04-14 23:43:54 +0530

    Add basic test setup

--- TwitterClone/tweets/tests.py
+++ TwitterClone/tweets/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
+from .models import Tweet
 # Create your tests here.
+
+User = get_user_model()
+
+
+class TweetModelTestCase(TestCase):
+    def setUp(self):
+        Tweet.objects.create(
+            user=User.objects.first(),
+            content='Some random content'
+        )
