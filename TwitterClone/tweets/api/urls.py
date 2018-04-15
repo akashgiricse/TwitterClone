f05d96646b1c4b7979b6af8c8e41a70b3a7b0d71
akashgiricse <akashgiricse@gmail.com>
2018-04-15 20:27:38 +0530

    Basic setup for api

---
+++ TwitterClone/tweets/api/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+# from .views import tweet_detail_view, tweet_list_view
+from django.views.generic.base import RedirectView
+from .views import (
+    # TweetDetailView,
+    # TweetListView,
+    # TweetCreateView,
+    # TweetUpdateView,
+    # TweetDeleteView
+)
+
+
+urlpatterns = [
+    # url(r'^$', RedirectView.as_view(url="/")),
+    # url(r'^search/$', TweetListView.as_view(), name='list'),
+    # url(r'^create/$', TweetCreateView.as_view(), name='create'),
+    # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
+    # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
+    # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),
+]
