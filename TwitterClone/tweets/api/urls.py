6e16a1545abd98617c2bbfb919c73c82928c3ecb
akashgiricse <akashgiricse@gmail.com>
2018-04-15 20:51:49 +0530

    Created first api for TweetList

--- TwitterClone/tweets/api/urls.py
+++ TwitterClone/tweets/api/urls.py
@@ -2,17 +2,13 @@ from django.conf.urls import url
 # from .views import tweet_detail_view, tweet_list_view
 from django.views.generic.base import RedirectView
 from .views import (
-    # TweetDetailView,
-    # TweetListView,
-    # TweetCreateView,
-    # TweetUpdateView,
-    # TweetDeleteView
+    TweetListAPIView
 )
 
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),
-    # url(r'^search/$', TweetListView.as_view(), name='list'),
+    url(r'^$', TweetListAPIView.as_view(), name='list'),
     # url(r'^create/$', TweetCreateView.as_view(), name='create'),
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
