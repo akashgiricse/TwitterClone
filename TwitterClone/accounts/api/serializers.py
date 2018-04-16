1556e74dc58736f65a2f5fdbb391efc0daffdb25
akashgiricse <akashgiricse@gmail.com>
2018-04-16 22:42:49 +0530

    Add SerializerMethod for follower_count

--- TwitterClone/accounts/api/serializers.py
+++ TwitterClone/accounts/api/serializers.py
@@ -7,11 +7,18 @@ User = get_user_model()
 
 
 class UserDisplaySerializer(serializers.ModelSerializer):
+
+    follower_count = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = [
 
             'username',
             'first_name',
-            'last_name'
+            'last_name',
+            'follower_count'
         ]
+
+    def get_follower_count(self, obj):
+        return 0
