4725adc7801485f28e97b5e011ec615f326e2922
akashgiricse <akashgiricse@gmail.com>
2018-05-14 23:33:33 +0530

    Add date_display, timesince in API

--- TwitterClone/tweets/api/serializers.py
+++ TwitterClone/tweets/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.utils.timesince import timesince
 from accounts.api.serializers import UserDisplaySerializer
 from tweets.models import Tweet
 
@@ -7,11 +7,21 @@ from tweets.models import Tweet
 class TweetModelSerializer(serializers.ModelSerializer):
 
     user = UserDisplaySerializer(read_only=True)
+    date_display = serializers.SerializerMethodField()
+    timesince = serializers.SerializerMethodField()
 
     class Meta:
         model = Tweet
         fields = [
             'user',
             'content',
-            'timestamp'
+            'timestamp',
+            'date_display',
+            'timesince',
         ]
+
+    def get_date_display(self, obj):
+        return obj.timestamp.strftime("%b %d %I:%M %p")
+
+    def get_timesince(self, obj):
+        return timesince(obj.timestamp) + " ago"
